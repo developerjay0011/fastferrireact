a3e7321d5a1fbd790d31bee6b222c42204e0af8b
developerjay0011 <86921677+developerjay0011@users.noreply.github.com>
2024-01-16 19:55:16 +0530

    commit latest

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,30 @@
+importScripts(
+    "https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js"
+);
+importScripts(
+    "https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js"
+);
+// // Initialize the Firebase app in the service worker by passing the generated config
+const firebaseConfig = {
+    apiKey: "AIzaSyBdrInzeSFI0f0fK4sEqI7ls9ZkQ0hmpnk",
+    authDomain: "fastferri-14625.firebaseapp.com",
+    projectId: "fastferri-14625",
+    storageBucket: "fastferri-14625.appspot.com",
+    messagingSenderId: "601491405853",
+    appId: "1:601491405853:web:a2f0ce8a78d1a770ed5af4",
+    measurementId: "G-WBWSKBZ9RV"
+};
+
+firebase?.initializeApp(firebaseConfig);
+
+// Retrieve firebase messaging
+const messaging = firebase?.messaging();
+
+messaging.onBackgroundMessage(function (payload) {
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
---
+++ src/firebase.js
@@ -0,0 +1,65 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import {
+  getMessaging,
+  getToken,
+  onMessage,
+  isSupported,
+} from "firebase/messaging";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdrInzeSFI0f0fK4sEqI7ls9ZkQ0hmpnk",
+  authDomain: "fastferri-14625.firebaseapp.com",
+  projectId: "fastferri-14625",
+  storageBucket: "fastferri-14625.appspot.com",
+  messagingSenderId: "601491405853",
+  appId: "1:601491405853:web:a2f0ce8a78d1a770ed5af4",
+  measurementId: "G-WBWSKBZ9RV"
+};
+const firebaseApp = !getApps().length
+  ? initializeApp(firebaseConfig)
+  : getApp();
+const messaging = (async () => {
+  try {
+    const isSupportedBrowser = await isSupported();
+    if (isSupportedBrowser) {
+      return getMessaging(firebaseApp);
+    }
+    return null;
+  } catch (err) {
+    return null;
+  }
+})();
+
+export const fetchToken = async (setTokenFound, setFcmToken) => {
+  return getToken(await messaging, {
+    vapidKey:
+      "",
+  })
+    .then((currentToken) => {
+      if (currentToken) {
+        setTokenFound(true);
+        setFcmToken(currentToken);
+
+        // Track the token -> client mapping, by sending to backend server
+        // show on the UI that permission is secured
+      } else {
+        setTokenFound(false);
+        setFcmToken();
+        // shows on the UI that permission is required
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      // catch error while creating client token
+    });
+};
+
+export const onMessageListener = async () =>
+  new Promise((resolve) =>
+    (async () => {
+      const messagingResolve = await messaging;
+      onMessage(messagingResolve, (payload) => {
+        resolve(payload);
+      });
+    })()
+  );
